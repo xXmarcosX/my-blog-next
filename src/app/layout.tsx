624f008cc8478607a960a97575d3d508b2ef00c3
xXmarcosX <marcos.v3567@gmail.com>
2025-07-25 13:43:18 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: 'My Blog'
+}
+
+export default function RootLayout({ children }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+       {children}
+      </body>
+    </html>
+  );
+}
