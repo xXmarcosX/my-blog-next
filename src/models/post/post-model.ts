880243f797d0ec3200c98798d23fbca24d6229ed
xXmarcosX <marcos.v3567@gmail.com>
2025-07-28 17:05:58 -0300

    Adição de model para os posts

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export type PostModel = {
+  id: string,
+  title: string,
+  slug: string,
+  excerpt: string,
+  content: string, 
+  coverImageUrl: string,
+  published: boolean,
+  createdAt: string,
+  updatedAt: string,
+  author: string
+}
