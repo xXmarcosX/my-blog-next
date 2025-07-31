f9f32d81acfdc12e71cf5ea334858a99ff87c887
xXmarcosX <marcos.v3567@gmail.com>
2025-07-31 20:37:41 -0300

    Adição de funções para formatação de data e hora

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,19 @@
+import { format, formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
+
+export function formatDateTime(rawDate: string): string {
+  const date = new Date(rawDate)
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+    locale: ptBR
+  })
+}
+
+export function formatRelativeDate(rawDate: string): string {
+  const date = new Date(rawDate)
+
+  return formatDistanceToNow(date, {
+    locale: ptBR,
+    addSuffix: true
+  })
+}
