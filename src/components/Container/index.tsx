6f8b6a2a8b0cd36cf1d7c0bdb006185a2a6b9e0c
xXmarcosX <marcos.v3567@gmail.com>
2025-07-31 00:01:30 -0300

    Ajuste no componente container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,8 +4,8 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:text-slate-100 dark:bg-neutral-900">
-      <div className="max-w-screen-lg mx-auto px-8">
+    <div className="text-slate-900 bg-slate-100 min-h-screen dark:text-slate-100 dark:bg-neutral-900 border-amber-50 w-full">
+      <div className="max-w-screen px-8 lg:px-38 mx-auto">
         {children}
       </div>
     </div>
