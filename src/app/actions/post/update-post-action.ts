1a2590c5a756838ff125bab9c8b081875b690621
xXmarcosX <marcos.v3567@gmail.com>
2026-05-01 12:07:37 -0300

    Adicionada server action para atualização de posts

---
+++ src/app/actions/post/update-post-action.ts
@@ -0,0 +1,80 @@
+'use server';
+
+import {
+  makePartialPublicPost,
+  makePublicPostFromDb,
+  PublicPost,
+} from '@/dto/post/dto';
+import { PostUpdateSchema } from '@/lib/post/validations/validatesPost';
+import { postRepository } from '@/repositories/post';
+import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
+import { revalidateTag } from 'next/cache';
+
+type UpdatePostActionState = {
+  formState: PublicPost;
+  errors: string[];
+  success?: boolean;
+};
+
+export async function updatePostAction(
+  prevState: UpdatePostActionState,
+  formData: FormData,
+): Promise<UpdatePostActionState> {
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ['Dados inválidos'],
+    };
+  }
+
+  const id = formData.get('id')?.toString() || '';
+
+  if (!id || typeof id !== 'string') {
+    return {
+      formState: prevState.formState,
+      errors: ['ID inválido'],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
+  const zodParsedObj = PostUpdateSchema.safeParse(formDataToObj);
+
+  if (!zodParsedObj.success) {
+    const errors = getZodErrorMessages(zodParsedObj.error);
+    return {
+      errors,
+      formState: makePartialPublicPost(formDataToObj),
+    };
+  }
+
+  const validPostData = zodParsedObj.data;
+  const newPost = {
+    ...validPostData,
+    updatedAt: new Date().toISOString(),
+  };
+
+  let post;
+  try {
+    post = await postRepository.update(id, newPost);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        formState: makePartialPublicPost(formDataToObj),
+        errors: [e.message],
+      };
+    }
+
+    return {
+      formState: makePartialPublicPost(formDataToObj),
+      errors: ['Erro desconhecido'],
+    };
+  }
+
+  revalidateTag('posts', '');
+
+  return {
+    formState: makePublicPostFromDb(post),
+    errors: [],
+    success: true,
+  };
+}
